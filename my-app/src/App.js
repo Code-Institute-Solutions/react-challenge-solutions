afd916d3b7c50e7ab2b43231df8f395911d2205f
John Long <johnlonggately@gmail.com>
2021-05-26 09:43:38 +0000

    changed text in app.js to a comment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,8 @@ import css from './App.module.css';
 function App() {
   return (
     <div className={css.App}>
-      Add your components here
+      {/* Add your components here */}
+      
     </div>
   );
 }
