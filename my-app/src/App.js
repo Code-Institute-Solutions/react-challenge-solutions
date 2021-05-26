0d67513c8b337369c60d76341351052ed185b6a9
John Long <johnlonggately@gmail.com>
2021-05-26 11:36:30 +0000

    Solution to Challenge 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarSimple />
       <Sidebar />
+
     </div>
   );
 }
