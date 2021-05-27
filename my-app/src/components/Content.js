eab6c62ed2dc756610b3b87b35a2eb8256cba82c
John Long <johnlonggately@gmail.com>
2021-05-27 08:09:48 +0000

    Solution to challenge 6

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import css from './css/Content.module.css';
 import {savedPosts} from '../posts.json';
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
     
     render() {
@@ -17,24 +30,12 @@ export class Content extends Component {
                 </div>
 
                 <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
 
-                    {/* Part 1: Creating the map function */}
-
-                        {/* {
-                    savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt='image'/>
-                                <p>{post.description}</p>
-                                </div>
-                        })
-                    } */}
-
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
                 </div>
             </div>
         )
