ed4219634535fa4ea87f6c85cd12a37508f88bed
John Long <johnlonggately@gmail.com>
2021-05-28 10:09:07 +0000

    Solution to challenge 9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from "./components/NavBarForm";
 import Content from './components/Content';
 import ContentHooks from './components/ContentHooks';
+import ContentAPI from './components/ContentAPI';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <NavBarForm />
       <Sidebar />
       {/* <Content /> */}
-      <ContentHooks />
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -4,8 +4,7 @@ import css from './css/PostItem.module.css';
 
 function PostItem(props) {
     return (
-        props.savedPosts.map(post => {
-            // Extra task destructuring 
+        props.savedPosts.map(post => { 
             const {title, name, image, description} = post
             return <div className={css.SearchItem} key={title}>
                 <p>{title}</p>
